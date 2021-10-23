c6e14aa0f3dba56c40ee3f77d2e10cfad33ffc12
NotorDOG <68875711+NotorDOG@users.noreply.github.com>
2021-10-23 10:33:17 -0400

    Add project files.

---
+++ Data/MVCProgramContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVCProgram.Models;
+
+namespace MVCProgram.Data
+{
+    public class MVCProgramContext : DbContext
+    {
+        public MVCProgramContext (DbContextOptions<MVCProgramContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCProgram.Models.Movie> Movie { get; set; }
+    }
+}
